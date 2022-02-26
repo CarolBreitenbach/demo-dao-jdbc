20a8253f6d4f4e676166b5ca203cc145f4f05c18
Caroline Breitenbach <carolinebreitenbach@gmail.com>
2022-02-26 16:40:00 -0300

    DepartmentDaoJDBC

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.IMPL.DepartmentDaoJDBC;
 import model.dao.IMPL.SellerDaoJDBC;
 
 public class DaoFactory {
@@ -8,4 +9,10 @@ public class DaoFactory {
 	public static SellerDaoJDBC createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
 	}
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
+	
+	
+	
 }
